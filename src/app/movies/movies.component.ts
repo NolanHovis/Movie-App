c608113378a69ccd5b0c26ba458174102289a052
Nolan Hovis <nolan.hovispersonal@gmail.com>
2021-12-09 18:02:01 -0600

    Changed form Init location

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -7,77 +7,83 @@ import { Movie } from './movie.model';
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.scss']
+  styleUrls: ['./movies.component.scss'],
 })
 export class MoviesComponent implements OnInit {
-  movieForm: FormGroup
-  newMovie = false
-  editMovie = false
-  addOrCancel = 'Add'
-  movies: Movie[] =[]
-  id: number
+  movieForm: FormGroup;
+  newMovie = false;
+  editMovie = false;
+  addOrCancel = 'Add';
+  movies: Movie[] = [];
+  id: number;
 
-  constructor(private movieService: MovieService, private route: ActivatedRoute) { }
+  constructor(
+    private movieService: MovieService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.fetchMovies()
+    this.fetchMovies();
 
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMovie = params['id'] != null;
-      this.initForm()
-    })
+      this.initForm();
+    });
+
+    this.movieService.newMovie.subscribe((data) => {
+      console.log(data);
+    });
   }
 
   fetchMovies() {
     this.movieService.getMovies().subscribe((data: any) => {
-      console.table(data.payload)
-      this.movies = data.payload
-    })
+      console.table(data.payload);
+      this.movies = data.payload;
+    });
   }
 
   onAddMovie() {
-    this.newMovie = !this.newMovie
+    this.newMovie = !this.newMovie;
     if (this.newMovie) {
-      this.addOrCancel = 'Cancel'
+      this.addOrCancel = 'Cancel';
     }
     if (!this.newMovie) {
-      this.addOrCancel = 'Add'
+      this.addOrCancel = 'Add';
     }
   }
 
   onSubmitMovie() {
-    const movieToAdd = this.movieForm
-    if(this.editMovie) {
+    const movieToAdd = this.movieForm;
+    if (this.editMovie) {
       //updating movie
+    } else {
+      this.movieService.newMovie.next(movieToAdd.value);
+      console.table(this.movieForm.value);
+      this.movieService.addMovie(movieToAdd.value);
     }
-    else {
-      console.table(this.movieForm.value)
-      this.movieService.addMovie(movieToAdd.value)
-    }
-    this.onAddMovie()
+
+    this.onAddMovie();
   }
 
   private initForm() {
-    let movieTitle = ''
-    let movieDirector = ''
-    let movieYear = ''
-    let movieIncome = ''
-    let movieDuration = ''
-    let movieRating = ''
+    let movieTitle = '';
+    let movieDirector = '';
+    let movieYear = '';
+    let movieIncome = '';
+    let movieDuration = '';
+    let movieRating = '';
 
     if (this.editMovie) {
-
     }
 
     this.movieForm = new FormGroup({
-      'title' : new FormControl(movieTitle, Validators.required),
-      'director' : new FormControl(movieDirector, Validators.required),
-      'year' : new FormControl(movieYear, Validators.required),
-      'income' : new FormControl(movieIncome, Validators.required),
-      'duration' : new FormControl(movieDuration, Validators.required),
-      'parental_rating' : new FormControl(movieRating, Validators.required)
-    })
+      title: new FormControl(movieTitle, Validators.required),
+      director: new FormControl(movieDirector, Validators.required),
+      year: new FormControl(movieYear, Validators.required),
+      income: new FormControl(movieIncome, Validators.required),
+      duration: new FormControl(movieDuration, Validators.required),
+      parental_rating: new FormControl(movieRating, Validators.required),
+    });
   }
-
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,28 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Movie } from '../movies/movie.model';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
-  movies: Movie[] = []
+  newMovie = new Subject<Movie>();
+  movies: Movie[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getMovies() {
-    return this.http
-      .get<Movie[]>('https://codelabs2021.herokuapp.com/api/v1/movies/index')
+    return this.http.get<Movie[]>(
+      'https://codelabs2021.herokuapp.com/api/v1/movies/index'
+    );
   }
 
   addMovie(movie: Movie) {
     this.http
-      .post<any>('https://codelabs2021.herokuapp.com/api/v1/movies/create', movie).subscribe({
-        next: data => {
-          movie.id = data.id
-        }
-      })
+      .post<Movie>(
+        'https://codelabs2021.herokuapp.com/api/v1/movies/create',
+        movie
+      )
+      .subscribe({
+        next: (data) => {
+          movie.id = data.id;
+        },
+      });
   }
 }
